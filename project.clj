f14544daa7d1228920bbaebbb339f25a6bbde151
Tibor Kiss <tibor.kiss@gmail.com>
2016-12-05 21:57:08 +0100

    travis-ci.org support

--- project.clj
+++ project.clj
@@ -13,4 +13,5 @@
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [kafka-clj "3.6.8"]
                  [ib-re-actor "0.1.8"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.0.0"]]}})
